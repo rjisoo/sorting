99cafd9f7b033612bcf8f5f7d2af5caf6e1ab906
rjisoo <jisoo.cc@gmail.com>
2016-09-15 15:58:28 -0400

    complete

--- mergesort.js
+++ mergesort.js
@@ -11,12 +11,20 @@ function merge(leftArr, rightArr) {
     else
       mergedArr.push(rightArr.shift());
   }
-
   return !leftArr.length ? mergedArr.concat(rightArr) : mergedArr.concat(leftArr);
 }
 
+// function mergeSort(array) {
+// 	if (array.length > 1) {
+// 		var splitArr = split(array);
+// 		return merge(mergeSort(splitArr[0]), mergeSort(splitArr[1])); 
+// 	} else {
+// 		return array;
+// 	}
+// }
+
+
 function mergeSort(array) {
-
-  /* your code here */
-
+	var splitArr = split(array);
+	return array.length > 1 ? merge(mergeSort(splitArr[0]), mergeSort(splitArr[1])) : array;
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -5,11 +5,13 @@ describe('Split Array function', function() {
   it('is able to split an odd length array into two halves', function() {
     expect(split([1,2,3,4,5])).toEqual([[1,2,3],[4,5]]);
   });
-
+  it('is able to split an 1 length array into two halves', function() {
+    expect(split([1])).toEqual([[1],[]]);
+  });
 
 });
 
-describe('Merge Sort', function(){
+describe('Merge', function(){
   it('is able to merge two sorted arrays when lopsided left', function(){
     expect(merge([1,2,3],[4,5])).toEqual([1,2,3,4,5]);
   });
@@ -19,4 +21,27 @@ describe('Merge Sort', function(){
   it('is able to merge two sorted arrays into one sorted array', function(){
     expect(merge([5,9,13],[1,2,8,20])).toEqual([1,2,5,8,9,13,20]);
   });
+  it('is able to merge two sorted arrays and one empty array into one sorted array', function(){
+    expect(merge([5,9,13],[])).toEqual([5,9,13]);
+  });
 });
+
+
+describe('Merge Sort', function(){
+  it('is able to sort reversed arrays', function(){
+    expect(mergeSort([5,4,3,2,1])).toEqual([1,2,3,4,5]);
+  });
+  it('is able to sort sorted arrays', function(){
+    expect(mergeSort([1,2,3,4,5])).toEqual([1,2,3,4,5]);
+  });
+  it('is able to sort empty arrays', function(){
+    expect(mergeSort([])).toEqual([]);
+  });
+  it('is able to sort random arrays', function(){
+    expect(mergeSort([20,1,13,5,2,8,9,7])).toEqual([1,2,5,7,8,9,13,20]);
+  });
+  it('is able to sort one element arrays', function(){
+    expect(mergeSort([7])).toEqual([7]);
+  });
+});
+
