1829984d20ef0ce1adac56fae268079df4f31719
Jake Peyser <jakepeyser@gmail.com>
2016-09-15 16:37:47 -0400

    mergesort extra credit

--- mergesort.js
+++ mergesort.js
@@ -3,28 +3,29 @@ function split(wholeArray) {
           wholeArray.slice(Math.ceil(wholeArray.length/2), wholeArray.length)];
 }
 
-function merge(leftArr, rightArr) {
-  var mergedArr = [];
-  while (leftArr.length && rightArr.length) {
-    if(leftArr[0] < rightArr[0])
-      mergedArr.push(leftArr.shift());
-    else
-      mergedArr.push(rightArr.shift());
-  }
-  return !leftArr.length ? mergedArr.concat(rightArr) : mergedArr.concat(leftArr);
+function merge(leftArr, rightArr, compare) {
+  // var mergedArr = [];
+  // while (leftArr.length && rightArr.length) {
+  //   if(compare(rightArr[0], leftArr[0]))
+  //     mergedArr.push(leftArr.shift());
+  //   else
+  //     mergedArr.push(rightArr.shift());
+  // }
+  // return !leftArr.length ? mergedArr.concat(rightArr) : mergedArr.concat(leftArr);
+  return leftArr.concat(rightArr).sort(compare);
 }
 
 // function mergeSort(array) {
 // 	if (array.length > 1) {
 // 		var splitArr = split(array);
-// 		return merge(mergeSort(splitArr[0]), mergeSort(splitArr[1])); 
+// 		return merge(mergeSort(splitArr[0]), mergeSort(splitArr[1]));
 // 	} else {
 // 		return array;
 // 	}
 // }
 
 
-function mergeSort(array) {
+function mergeSort(array, compare) {
 	var splitArr = split(array);
-	return array.length > 1 ? merge(mergeSort(splitArr[0]), mergeSort(splitArr[1])) : array;
+	return array.length > 1 ? merge(mergeSort(splitArr[0], compare), mergeSort(splitArr[1], compare), compare) : array;
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -12,36 +12,70 @@ describe('Split Array function', function() {
 });
 
 describe('Merge', function(){
+
+  var compare;
+  beforeEach(function() {
+    compare =  {
+      func: function(a,b) {
+        if (a < b) return -1; // returning `-1` means "a goes before b"
+        if (a > b) return 1;  // returning  `1` means "b goes before a"
+        return 0; // returning 0 means "a and b are equivalent"
+      }
+    };
+  });
+
   it('is able to merge two sorted arrays when lopsided left', function(){
-    expect(merge([1,2,3],[4,5])).toEqual([1,2,3,4,5]);
+    expect(merge([1,2,3],[4,5], compare.func)).toEqual([1,2,3,4,5]);
   });
   it('is able to merge two sorted arrays when lopsided right', function(){
-    expect(merge([4,5],[1,2,3])).toEqual([1,2,3,4,5]);
+    expect(merge([4,5],[1,2,3], compare.func)).toEqual([1,2,3,4,5]);
   });
   it('is able to merge two sorted arrays into one sorted array', function(){
-    expect(merge([5,9,13],[1,2,8,20])).toEqual([1,2,5,8,9,13,20]);
+    spyOn(compare, 'func').and.callThrough();
+    expect(merge([5,9,13],[1,2,8,20], compare.func)).toEqual([1,2,5,8,9,13,20]);
+    //expect(compare.func).toHaveBeenCalled();
   });
   it('is able to merge two sorted arrays and one empty array into one sorted array', function(){
-    expect(merge([5,9,13],[])).toEqual([5,9,13]);
+    expect(merge([5,9,13],[], compare.func)).toEqual([5,9,13]);
   });
 });
 
 
 describe('Merge Sort', function(){
+
+  var compare;
+  beforeEach(function() {
+    compare =  {
+      func: function(a,b) {
+        if (a < b) return -1; // returning `-1` means "a goes before b"
+        if (a > b) return 1;  // returning  `1` means "b goes before a"
+        return 0; // returning 0 means "a and b are equivalent"
+      }
+    };
+  });
+
   it('is able to sort reversed arrays', function(){
-    expect(mergeSort([5,4,3,2,1])).toEqual([1,2,3,4,5]);
+    expect(mergeSort([5,4,3,2,1], compare.func)).toEqual([1,2,3,4,5]);
   });
   it('is able to sort sorted arrays', function(){
-    expect(mergeSort([1,2,3,4,5])).toEqual([1,2,3,4,5]);
+    expect(mergeSort([1,2,3,4,5], compare.func)).toEqual([1,2,3,4,5]);
   });
   it('is able to sort empty arrays', function(){
-    expect(mergeSort([])).toEqual([]);
+    expect(mergeSort([], compare.func)).toEqual([]);
   });
   it('is able to sort random arrays', function(){
-    expect(mergeSort([20,1,13,5,2,8,9,7])).toEqual([1,2,5,7,8,9,13,20]);
+    expect(mergeSort([20,1,13,5,2,8,9,7], compare.func)).toEqual([1,2,5,7,8,9,13,20]);
   });
   it('is able to sort one element arrays', function(){
-    expect(mergeSort([7])).toEqual([7]);
+    expect(mergeSort([7], compare.func)).toEqual([7]);
+  });
+  it('is able to sort array of objects by age', function(){
+    compare.func = function(a,b) {
+      if (a.age < b.age) return -1; // returning `-1` means "a goes before b"
+      if (a.age > b.age) return 1;  // returning  `1` means "b goes before a"
+      return 0; // returning 0 means "a and b are equivalent"
+    };
+    expect(mergeSort([{age:5},{age:2},{age:4}], compare.func)).toEqual([{age:2},{age:4},{age:5}]);
   });
 });
 
